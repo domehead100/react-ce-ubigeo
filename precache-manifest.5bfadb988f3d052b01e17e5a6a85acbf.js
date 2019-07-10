0de7846674997be9e2a6239736a262274099a99d
Myhtssu Gustavo Garcia Millasaky <mggarcia@belatrix.com>
2019-07-10 10:31:32 -0500

    Updates

---
+++ precache-manifest.5bfadb988f3d052b01e17e5a6a85acbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b3218d32f5f2462e9894c77c15b03b",
+    "url": "/react-ce-ubigeo/index.html"
+  },
+  {
+    "revision": "2eb6d4cfd0edb32e4e31",
+    "url": "/react-ce-ubigeo/static/css/main.f0b327fa.chunk.css"
+  },
+  {
+    "revision": "419846e86889091933df",
+    "url": "/react-ce-ubigeo/static/js/2.da6cf4bc.chunk.js"
+  },
+  {
+    "revision": "2eb6d4cfd0edb32e4e31",
+    "url": "/react-ce-ubigeo/static/js/main.58ff4bc7.chunk.js"
+  },
+  {
+    "revision": "2fc9c9a9ab10c7b40157",
+    "url": "/react-ce-ubigeo/static/js/runtime~main.bfb28c53.js"
+  },
+  {
+    "revision": "5b118caf9aae660e15e5e5865285ca8c",
+    "url": "/react-ce-ubigeo/static/media/cloud.5b118caf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-ce-ubigeo/precache-manifest.5bfadb988f3d052b01e17e5a6a85acbf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ce-ubigeo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
